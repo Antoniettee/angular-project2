3b774a356f9ac52255e187ec8ba71645062c6ab9
Antoniettee <97518030+Antoniettee@users.noreply.github.com>
2022-01-12 16:57:33 +0100

    new one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import {RouterModule} from "@angular/router";
+import { MatTableModule} from "@angular/material/table";
+import { FormComponent } from './form/form.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
